3f68f236322247eec618c9c71654c0bcba729558
Selvi <slvkesuma@gmail.com>
2022-01-25 16:13:35 +0200

    updated header file

---
+++ fillit.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skesuma <skesuma@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 16:12:32 by skesuma           #+#    #+#             */
+/*   Updated: 2022/01/25 16:13:15 by skesuma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include <stdlib.h>
+# include <stdio.h> //remove later?
+# include "libft/libft.h"
+
+int	get_next_line(const int fd, char **line);
+
+#endif
