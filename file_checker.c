14ee4dabfee36cf8fe07a47c93ee9d88b15e99e8
Selvi <slvkesuma@gmail.com>
2022-02-07 17:34:23 +0200

    Fixed final_line check

--- file_checker.c
+++ file_checker.c
@@ -6,87 +6,99 @@
 /*   By: skesuma <skesuma@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 11:36:00 by skesuma           #+#    #+#             */
-/*   Updated: 2022/02/04 14:06:44 by skesuma          ###   ########.fr       */
+/*   Updated: 2022/02/07 17:33:44 by skesuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+/*
+checks that: 1. there are exactly 21 lines for each grid.
+2. there's \n at every 20th array item.
+3. there's \n at the end of each line.
+4. there's no other char except '.', '#' and '\n'.
+*/
 int	format_checker(int ret, char *buf)
 {
 	int	i;
 
 	buf[ret] = '\0';
 	if ((ret + 1) % 21 != 0 || ret == 0 || ret == -1 || ret > 545)
-		return (write(1, "Invalid no. of lines or read failed\n", 36));
+		return (-1);
 	i = 0;
 	if (buf[i] != '.' && buf[i] != '#')
-		return (write(1, "Invalid block\n", 14));
+		return (-1);
 	while (buf[++i] != '\0')
 	{
 		if (i % 21 == 0 && buf[i - 1] != '\n')
-			return (write(1, "Invalid line break\n", 20));
-		else if (i % 21 % 5 == 0 && buf[i - 1] != '\n') //\n at the end of each line
-			return (write(1, "Invalid in-grid line break\n", 27));
+			return (-1);
+		else if ((i % 21) % 5 == 0 && buf[i - 1] != '\n')
+			return (-1);
 		else if (buf[i] != '.' && buf[i] != '#' && buf[i] != '\n')
-			return (write(1, "Invalid block\n", 14));
+			return (-1);
 	}
 	return (i);
 }
 
-// int	touching(int i, char *buf, int x, int y) //checks 21 lines at a time
-// {
-// 	while (++y < i)
-// 	{
-// 		if (buf[y] == '#')
-// 		{
-// 			while (++x < 4)
-// 			{
-// 				if (buf[y + 1] == '#')
-// 					y += 1;
-// 				else if (buf[y + 5] == '#')
-// 					y += 5;
-// 				else if (buf[y + 1] == '#' && buf[y - 1] == '#')
-// 				{
-// 					y += 1;
-// 					x += 2;
-// 				}
-// 				else if (buf[y - 1] == '#')
-// 					y++;
-// 				else
-// 					return (write(1, "invalid tetros", 1));
-// 			}
-// 		}
-// 		else if ((y + 1) % 21 == 0 && x != 4)
-// 			return (-1);
-// 	}
-// 	return (0);
-// }
+/*
+Executed specifically on the last line of a tetros using call by reference
+in order to solve out-of-range issues, and also eliminating the need to return
+multiple values as we're changing the variable's value by accessing them via
+address instead of passing copies of them.
+*/
+void	touching_lastline(int *y, char *buf, int *t2, int *t1)
+{
+	if (buf[*y] == '#')
+	{
+		if (buf[*y + 1] == '#' && buf[*y - 1] == '#')
+			(*t2)++;
+		else if ((buf[*y + 1] == '#' || buf[*y - 1] == '#') && \
+				(buf[*y - 5] == '#'))
+			(*t2)++;
+		else if (buf[*y + 1] == '#' || buf[*y - 1] == '#' || \
+				buf[*y - 5] == '#')
+			(*t1)++;
+		else
+			*y = 21;
+	}
+}
 
-int	touching(int touched2, char *buf, int touched1, int y) //checks 21 lines at a time
+/*
+t2 = touching 2 blocks. t1 = touching 1 block.
+checking how many blocks touch each other and fulfil the conditions for
+a valid tetros.
+*/
+int	touching(int t2, char *buf, int t1, int y)
 {
 	while (++y < 21)
 	{
 		if (buf[y] == '#')
 		{
-			if (buf[y + 5] == '#' && buf[y - 5] == '#')
-				touched2++;
+			if (y > 14)
+				touching_lastline(&y, buf, &t2, &t1);
+			else if (buf[y + 5] == '#' && buf[y - 5] == '#')
+				t2++;
 			else if (buf[y + 1] == '#' && buf[y - 1] == '#')
-				touched2++;
-			else if ((buf[y + 1] == '#' || buf[y - 1] == '#') && (buf[y + 5] == '#' || buf[y - 5] == '#'))
-				touched2++;
-			else if (buf[y + 1]  == '#'|| buf[y - 1]  == '#' ||buf [y + 5]  == '#' || buf[y - 5]  == '#')
-				touched1++;
+				t2++;
+			else if ((buf[y + 1] == '#' || buf[y - 1] == '#') && \
+					(buf[y - 5] == '#' || buf[y + 5] == '#'))
+				t2++;
+			else if (buf[y + 1] == '#' || buf[y - 1] == '#' || \
+					buf [y + 5] == '#' || buf[y - 5] == '#')
+				t1++;
 			else
-				return (write(1, "invalid tetros\n", 15));
+				return (-1);
 		}
 	}
-	if (touched2 != 2 && touched1 != 2)
-		return (write(1, "invalid tetros\n", 15));
-	return (0);
+	if ((t1 == t2) || (t1 == 3 && t2 == 1) || (t1 == 0 && t2 == 4))
+		return (0);
+	return (-1);
 }
 
-int block_checker(int i, char *buf, int fd, int ret)
+/*
+checks that for every 21 chars, there are four '#'.
+*/
+int	block_checker(int i, char *buf, int fd, int ret)
 {
 	int	y;
 	int	x;
@@ -99,8 +111,6 @@ int block_checker(int i, char *buf, int fd, int ret)
 			x++;
 		if ((y + 1) % 21 == 0 && x != 4)
 		{
-			ft_putstr("invalid number of # at line\n");
-			ft_putnbr(y);
 			return (-1);
 		}
 		else if ((y + 1) % 21 == 0 && x == 4)
@@ -109,24 +119,25 @@ int block_checker(int i, char *buf, int fd, int ret)
 	}
 	y = -1;
 	i = 0;
-	while (read(fd, buf, 21) && ret != -1 )
+	while (read(fd, buf, 21) && ret != -1)
+	{
+		buf[21] = '\0';
 		ret = touching(i, buf, x, y);
+	}
 	return (ret);
 }
 
-int	main(int argc, char *argv[])
+int	lets_check(char *argv[])
 {
-	int		fd;
 	int		check;
+	int		fd;
 	char	buf[546];
 	int		ret;
 
-	if (argc != 2)
-		return (write(1, "Usage: ./fillit input_file", 26));
 	fd = open(argv[1], O_RDONLY);
 	ret = read(fd, buf, 546);
 	if (fd == -1 || ret == -1)
-			return (-1);
+		return (-1);
 	check = format_checker(ret, buf);
 	if (check == -1)
 		return (-1);
@@ -135,5 +146,16 @@ int	main(int argc, char *argv[])
 	fd = open(argv[1], O_RDONLY);
 	check = block_checker(check, buf, fd, ret);
 	if (check == -1)
-		return (write(1, "invalid tetro\n", 14));
+		return (-1);
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 2)
+		return (write(1, "Usage: ./fillit input_file\n", 27));
+	if (lets_check(argv) != 0)
+		return (printf("invalid file\n"));
+	else
+		return (printf("valid file\n"));
 }
